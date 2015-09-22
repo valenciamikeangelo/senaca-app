cceac89c5cb05509870e2e5d25dd9f6310a9b740
valencia.mikeangelo@gmail.com <valencm@VALENCM.caimsc.compaid.com>
2015-09-22 13:35:27 +0800

    update

--- server/launcher.js
+++ server/launcher.js
@@ -8,8 +8,8 @@ services.forEach(function(service){
   //var log  = fs.createWriteStream('./log/'+service+'.log')
   var proc = spawn('node', ['./server/services/'+service+'.js','--seneca.log.all'])
 
-  proc.stdout.pipe(log)
-  proc.stderr.pipe(log)
+  //proc.stdout.pipe(log)
+  //proc.stderr.pipe(log)
 
   proc.stdout.pipe(process.stdout)
   proc.stderr.pipe(process.stderr)
