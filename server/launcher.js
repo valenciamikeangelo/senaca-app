a951931ce224f2b5cee2d70f04100e1b65de2ef8
valencia.mikeangelo@gmail.com <valencm@VALENCM.caimsc.compaid.com>
2015-09-22 13:02:41 +0800

    updated for heroku deployment

--- server/launcher.js
+++ server/launcher.js
@@ -5,8 +5,8 @@ var spawn = require('child_process').spawn
 var services = ['web-app','profile-service']
 
 services.forEach(function(service){
-  var log  = fs.createWriteStream('../log/'+service+'.log')
-  var proc = spawn('node', ['../server/services/'+service+'.js','--seneca.log.all'])
+  var log  = fs.createWriteStream('./log/'+service+'.log')
+  var proc = spawn('node', ['./server/services/'+service+'.js','--seneca.log.all'])
 
   proc.stdout.pipe(log)
   proc.stderr.pipe(log)
