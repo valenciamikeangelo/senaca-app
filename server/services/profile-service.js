8af85b45d7cc93df33ba3d19a5ba166fef177da9
valencia.mikeangelo@gmail.com <valencm@VALENCM.caimsc.compaid.com>
2015-09-22 12:42:02 +0800

    connect to mongolab

--- server/services/profile-service.js
+++ server/services/profile-service.js
@@ -3,7 +3,8 @@ var seneca     = require('seneca')()
 var mongoose = require('mongoose');
 
 require('../models/Profiles');
-mongoose.connect('mongodb://localhost/wisapp');
+//mongoose.connect('mongodb://localhost/wisapp');
+mongoose.connect('mongodb://valenciamikeangelo:devpass@ds051843.mongolab.com:51843/wisapp');
 
 seneca
   .use('../api/profile-api')
--- server/services/web-app.js
+++ server/services/web-app.js
@@ -8,7 +8,8 @@ var mongoose = require('mongoose');
 require('../models/Users');
 require('../config/passport');
 
-mongoose.connect('mongodb://localhost/wisapp');
+mongoose.connect('mongodb://valenciamikeangelo:devpass@ds051843.mongolab.com:51843/wisapp');
+//mongoose.connect('mongodb://localhost/wisapp');
 seneca.client({host: "localhost",port:10202,pin:{role:"profile",cmd:"*"}})
 
 require('./web/seneca-commands')(seneca);
